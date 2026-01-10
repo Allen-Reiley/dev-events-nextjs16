27d233243174877f162a7c7ebc65a7554ba6536d
Allen-Reiley <rbadigital093@gmail.com>
2026-01-10 19:30:31 +0200

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,53 @@
+export const EventsItem = [
+  {
+    title: 'React Conf 2025',
+    image: '/images/event1.png',
+    slug: 'react-conf-2025',
+    location: 'Las Vegas, NV',
+    date: 'March 15-16, 2025',
+    time: '9:00 AM - 6:00 PM'
+  },
+  {
+    title: 'PyCon US 2025',
+    image: '/images/event2.png',
+    slug: 'pycon-us-2025',
+    location: 'Pittsburgh, PA',
+    date: 'April 23-30, 2025',
+    time: '9:00 AM - 5:00 PM'
+  },
+  {
+    title: 'AWS re:Invent',
+    image: '/images/event3.png',
+    slug: 'aws-reinvent-2025',
+    location: 'Las Vegas, NV',
+    date: 'December 1-5, 2025',
+    time: '8:00 AM - 7:00 PM'
+  },
+  {
+    title: 'Google I/O',
+    image: '/images/event4.png',
+    slug: 'google-io-2025',
+    location: 'Mountain View, CA',
+    date: 'May 14-15, 2025',
+    time: '10:00 AM - 6:00 PM'
+  },
+  {
+    title: 'Hack the North',
+    image: '/images/event5.png',
+    slug: 'hack-the-north-2025',
+    location: 'Waterloo, ON',
+    date: 'September 13-15, 2025',
+    time: '48 Hour Hackathon'
+  },
+  {
+    title: 'Microsoft Build',
+    image: '/images/event6.png',
+    slug: 'microsoft-build-2025',
+    location: 'Seattle, WA',
+    date: 'May 20-22, 2025',
+    time: '9:00 AM - 5:00 PM'
+  }
+];
+
+const events = EventsItem;
+export default events
